60dffdbcf368ac9d368f1af264b0d0a5d8c92a79
lilyynyang <yyn605@outlook.com>
2020-10-21 01:05:23 +0800

    fixbug: customized user registration, password not hashed

--- hotzone/customauth/admin.py
+++ hotzone/customauth/admin.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.admin import admin
+from django.contrib.auth.admin import admin, UserAdmin
 from .models import Staff
 
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'staffID', 'is_active')
 
 admin.site.register(Staff, CustomUserAdmin)
