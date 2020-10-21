ac66f11e04c1d145149b99306b430f35008d2d9f
lilyynyang <yyn605@outlook.com>
2020-10-21 19:21:36 +0800

    feature: create new patient & links in patients

--- hotzone/records/urls.py
+++ hotzone/records/urls.py
@@ -10,7 +10,13 @@ urlpatterns = [
 	path('patient/<int:id>',
 		views.PatientDetailView.as_view(),
 		name='patient-detail'),
+	path('patient/new',
+		views.PatientCreateView.as_view(),
+		name='patient-new'),
 	path('patient/edit/<int:id>',
 		views.PatientEditView.as_view(),
 		name='patient-edit'),
+	path('patient/delete/<int:id>',
+		views.PatientDeleteView.as_view(),
+		name='patient-delete'),
 ]
--- hotzone/records/views.py
+++ hotzone/records/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView, TemplateView
-from django.views.generic.edit import UpdateView
+from django.views.generic import ListView, TemplateView, View
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Patient, Virus, Location, Case, Visit
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -16,10 +16,29 @@ class PatientDetailView(TemplateView):
 	template_name = "record/patient_detail.html"
 
 	def get_context_data(self, **kwargs):
-		patient = Patient.objects.get(pk=self.kwargs['id'])
+		patient = get_object_or_404(Patient, pk=self.kwargs['id'])
 		context = super().get_context_data()
 		context['patient'] = patient
 		return context
+
+class PatientCreateView(CreateView):
+	template_name = "record/patient_new.html"
+	model = Patient
+	fields = '__all__'
+	
+	@method_decorator(login_required)
+	def dispatch(self, *args, **kwargs):
+		return super().dispatch(*args, **kwargs)
+
+	def form_valid(self, form):
+		patient = form.save()
+		messages.success(self.request, 'Data saved. Creation successfully.')
+	
+		return HttpResponseRedirect(reverse('records:patient-detail', kwargs={'id':patient.pk}))
+
+	def form_invalid(self, form):
+		messages.error(self.request, 'Something is wrong. Creation fails.')
+		return super().form_invalid(form)
 
 class PatientEditView(UpdateView):
 	template_name = "record/patient_edit.html"
@@ -35,15 +54,25 @@ class PatientEditView(UpdateView):
 
 	def form_valid(self, form):
 		if form.has_changed():
-			form.save()
+			patient = form.save()
 			messages.success(self.request, 'Data saved. Edit successfully.')
 		else:
 			messages.info(self.request, 'Data saved. Nothing has changed')
-		return HttpResponseRedirect(self.get_success_url())
+		return HttpResponseRedirect(reverse('records:patient-detail', kwargs={'id':patient.pk}))
 
 	def form_invalid(self, form):
-		messages.error(request, 'Something is wrong. Edition fails.')
-		return super(form_invalid, form)
+		messages.error(self.request, 'Something is wrong. Edition fails.')
+		return super().form_invalid(form)
 
-	def get_success_url(self):
-		return reverse('records:patient-detail', kwargs={'id':self.get_object().pk})
+class PatientDeleteView(View):
+	@method_decorator(login_required)
+	def dispatch(self, *args, **kwargs):
+		return super().dispatch(*args, **kwargs)
+
+	def get(self, request, *args, **kwargs):
+		patient = get_object_or_404(Patient, pk=self.kwargs['id'])
+		patient.delete()
+		messages.success(request, 'Delete Successfully.')
+		return HttpResponseRedirect(reverse('records:patients'))
+
+
