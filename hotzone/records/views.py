e96e7eee857fa1dfa6e690bc5e8746f13fd6c859
lilyynyang <yyn605@outlook.com>
2020-10-21 19:47:55 +0800

    refactor: change route and page display

--- hotzone/records/views.py
+++ hotzone/records/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView, TemplateView, View
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic import ListView, TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Patient, Virus, Location, Case, Visit
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -12,8 +12,8 @@ class PatientAllView(ListView):
 	template_name = "record/patient_index.html"
 	model = Patient
 
-class PatientDetailView(TemplateView):
-	template_name = "record/patient_detail.html"
+class PatientShowView(TemplateView):
+	template_name = "record/patient_show.html"
 
 	def get_context_data(self, **kwargs):
 		patient = get_object_or_404(Patient, pk=self.kwargs['id'])
@@ -34,7 +34,7 @@ class PatientCreateView(CreateView):
 		patient = form.save()
 		messages.success(self.request, 'Data saved. Creation successfully.')
 	
-		return HttpResponseRedirect(reverse('records:patient-detail', kwargs={'id':patient.pk}))
+		return HttpResponseRedirect(reverse('records:patient-show', kwargs={'id':patient.pk}))
 
 	def form_invalid(self, form):
 		messages.error(self.request, 'Something is wrong. Creation fails.')
@@ -58,21 +58,27 @@ class PatientEditView(UpdateView):
 			messages.success(self.request, 'Data saved. Edit successfully.')
 		else:
 			messages.info(self.request, 'Data saved. Nothing has changed')
-		return HttpResponseRedirect(reverse('records:patient-detail', kwargs={'id':patient.pk}))
+		return HttpResponseRedirect(reverse('records:patient-show', kwargs={'id':self.get_object().pk}))
 
 	def form_invalid(self, form):
 		messages.error(self.request, 'Something is wrong. Edition fails.')
 		return super().form_invalid(form)
 
-class PatientDeleteView(View):
+class PatientDeleteView(DeleteView):
+	success_message = 'Delete Successfully.'
+	model = Patient
+
 	@method_decorator(login_required)
 	def dispatch(self, *args, **kwargs):
 		return super().dispatch(*args, **kwargs)
 
-	def get(self, request, *args, **kwargs):
-		patient = get_object_or_404(Patient, pk=self.kwargs['id'])
-		patient.delete()
-		messages.success(request, 'Delete Successfully.')
-		return HttpResponseRedirect(reverse('records:patients'))
+	def get_object(self, queryset=None):
+		return get_object_or_404(self.model, pk=self.kwargs['id'])
+
+	def get_success_url(self):
+		return reverse('records:patients')
+	
+	def get(self, *args, **kwargs):
+		return self.post(*args, **kwargs)
 
 
