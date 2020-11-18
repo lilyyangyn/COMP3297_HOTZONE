b4b45f4e7a924bcf3f8d10171b2d711b42807b31
lilyynyang <yyn605@outlook.com>
2020-11-18 23:28:41 +0800

    enable production database settings

--- hotzone/hotzone_config/settings/base.py
+++ hotzone/hotzone_config/settings/base.py
@@ -94,7 +94,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '',
     },
-    #'prod': env.dj_db_url('DATABASE_URL'),
+    'prod': env.dj_db_url('DATABASE_URL'),
 }
 
 # customized user model
