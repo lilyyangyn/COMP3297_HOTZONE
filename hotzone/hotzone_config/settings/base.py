95950847a13d7971b38826f8a1aa76ac11e8089c
lilyynyang <yyn605@outlook.com>
2020-11-21 16:17:13 +0800

    update email backend

--- hotzone/hotzone_config/settings/base.py
+++ hotzone/hotzone_config/settings/base.py
@@ -34,11 +34,11 @@ ALLOWED_HOSTS = ['dry-mesa-46974.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Add Email smtp configuration
 EMAIL_BACKEND   = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST      = 'smtp.gmail.com'
-EMAIL_PORT      = 587
+EMAIL_HOST      = 'smtp.163.com'
+EMAIL_PORT      = 465
 EMAIL_USE_TLS   = True
-EMAIL_HOST_USER = 'ericcsr666@gmail.com'
-EMAIL_HOST_PASSWORD = 'Kmcsr199871'
+EMAIL_HOST_USER = 'hotzone3297@163.com'
+EMAIL_HOST_PASSWORD = 'comp3297'
 
 
 
