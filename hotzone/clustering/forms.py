323abb418c72a7e0dbf5ae3da095f3576e091a53
h3553493 <47234665+h3553493@users.noreply.github.com>
2020-12-01 18:13:05 +0800

    Update forms.py

--- hotzone/clustering/forms.py
+++ hotzone/clustering/forms.py
@@ -16,3 +16,13 @@ class ClusterCreateForm(forms.Form):
 		required=True,
 		initial=2,
 	)
+	
+	def clean(self):
+		if not self.is_valid():
+			raise forms.ValidationError(u"You need to fill in all blanks")
+		elif self.cleaned_data['timeThres']==0:
+			self.add_error('timeThres',"timeThres cannot be zero. Please input valid timeThres.")
+			raise forms.ValidationError(u"timeThres cannot be zero. Please input valid timeThres.")
+		else:
+			cleaned_data = super(ClusterCreateForm, self).clean()
+		return cleaned_data
